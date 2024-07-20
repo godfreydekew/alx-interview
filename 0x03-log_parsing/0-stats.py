cc60e64024ae8deae1599a932b06291c68233110
godfreydekew <dekewgodfrey@gmail.com>
2024-07-20 09:28:06 +0000

    Update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,70 @@
 #!/usr/bin/python3
-"""
-log parsing
-"""
-
+'''
+Reads line by line from the stdin and computes metrics
+'''
 import sys
 import re
 
 
-def output(log: dict) -> None:
-    """
-    helper function to display stats
-    """
-    print("File size: {}".format(log["file_size"]))
-    for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
+def initialize_log():
+    '''
+    Initializes log dictionary and status code list
+    '''
+
+    status_code = [200, 301, 400, 401, 403, 404, 405, 500]
+    log = {"file_size": 0, "code_list": {str(code): 0 for code in status_code}}
+    return log
+
+
+def parse_line(line, regex, log):
+    '''
+    Uses regular expression to extract status code and file size
+    '''
+
+    match = regex.fullmatch(line)
+    if match:
+        stat_code, file_size = match.group(1, 2)
+        log["file_size"] += int(file_size)
+
+        if stat_code.isdecimal():
+            log["code_list"][stat_code] += 1
+
+    return log
+
+
+def print_codes(log):
+    '''
+    Prints File size and status code count
+    '''
+
+    print("File size: {}".format(log['file_size']))
+    sorted_code_list = sorted(log["code_list"])
+
+    for code in sorted_code_list:
+        if log["code_list"][code]:
+            print(f"{code}: {log['code_list'][code]}")
+
+
+def main():
+    '''Creates the regular expression and reads from the stdin'''
+
+    regex = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET '
+                       r'/projects/260 HTTP/1.1" (.{3}) (\d+)')
+    log = initialize_log()
+    line_count = 0
+
+    for line in sys.stdin:
+        line = line.strip()
+
+        line_count = line_count + 1
+        parse_log = parse_line(line, regex, log)
+
+        if line_count % 10 == 0:
+            print_codes(parse_log)
 
 
 if __name__ == "__main__":
-    regex = re.compile(
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+    '''Calling necessary functions'''
 
-    line_count = 0
-    log = {}
-    log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500]}
-
-    try:
-        for line in sys.stdin:
-            line = line.strip()
-            match = regex.fullmatch(line)
-            if (match):
-                line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
-
-                # File size
-                log["file_size"] += file_size
-
-                # status code
-                if (code.isdecimal()):
-                    log["code_frequency"][code] += 1
-
-                if (line_count % 10 == 0):
-                    output(log)
-    finally:
-        output(log)
+    main()
+    initialize_log()
