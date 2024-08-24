41b632185badcaafc774bc5732cca80eab0e341a
godfreydekew <dekewgodfrey@gmail.com>
2024-08-24 21:59:50 +0300

    Task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,7 +10,6 @@ def makeChange(coins: List[int], total: int) -> int:
         return 0
     track_coins = [total + 1] * (total + 1)
     track_coins[0] = 0
-    max_coins_needed = 0
     for i in range(1, total + 1):
         for coin in coins:
             if coin <= i:
