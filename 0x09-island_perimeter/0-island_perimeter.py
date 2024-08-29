99f580713738ad61c090db864109a5bcdfbc6cb7
godfreydekew <dekewgodfrey@gmail.com>
2024-08-29 13:16:39 +0300

    Update

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,8 +8,14 @@ def island_perimeter(grid):
     if len(grid) < 3 and isinstance(grid, list):
         return 0
     total_sum = 0
-    transposed_matrix = list(map(list, zip(*grid)))
-    if (sum(grid[0]) + sum(grid[-1])) + sum(transposed_matrix[0]) + sum(transposed_matrix[-1]) == 0:
-        for land in grid:
-            total_sum += sum(land)
-        return (total_sum * 2) + 2
+    for i in range(1, len(grid) - 1):
+        for j in range(1, len(grid[i]) - 1):
+            if grid[i][j] == 1:
+                sides_sum = grid[i][j - 1] + grid[i][j + 1] + (
+                        grid[i - 1][j] + grid[i + 1][j])
+                perimeter_sum = 4 - sides_sum
+                if perimeter_sum == 4:
+                    return 4
+                else:
+                    total_sum += perimeter_sum
+    return total_sum
